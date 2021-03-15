6c73a8a281028a089e35c8cfb61f9b77a93d5c4b
Virij80@gmail.com <kbpf2005>
2021-03-15 19:02:28 +0300

    Добавьте файлы проекта.

---
+++ HomeWork15.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+void Print_num(int N, int k)
+{
+    for (int i = k; i <= N; i += 2)
+    std::cout << i << " ";
+    std::cout << "\n";
+}
+
+int main()
+{
+    int N;
+    std::cout << "N="; std::cin >> N;
+
+    std::cout << "Even:\n";
+    Print_num(N, 0);
+
+    std::cout << "Odd:\n";
+    Print_num(N, 1);
+       
+    return 0;
+}
